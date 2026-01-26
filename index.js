c81ba68ff7c7844f3261a1f9a875e7e407871c4f
nilthing9 <44004681+nilthing9@users.noreply.github.com>
2026-01-26 17:37:45 +0000

    Updated index.js
    trying to fix some bits that dont yet work

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const dgram = require('dgram');
-const os = require('os');
+let Service, Characteristic;
 
-let Accessory, Service, Characteristic, UUIDGen;
+module.exports = (api) => {
+  Service = api.hap.Service;
+  Characteristic = api.hap.Characteristic;
+
+  api.registerPlatform(
+    'homebridge-mysqueezebox-v2',   // must match package.json name
+    'LMSPlatform',                  // must match config.json platform
+    LMSPlatform
+  );
+};
 
 class LMSPlatform {
   constructor(log, config, api) {
@@ -11,86 +19,28 @@ class LMSPlatform {
     this.config = config || {};
     this.api = api;
 
-    this.name = this.config.name || 'Lyrion Music Server';
-    this.autoDiscover = this.config.autoDiscover !== false; // default true
-    this.lmsHost = this.config.lmsHost || null;
-    this.lmsPort = this.config.lmsPort || 9000;
-    this.debug = this.config.debug || false;
-
-    this.players = [];
-
-    this.log.info('LMSPlatform initialising...');
-    this.log.info(`AutoDiscover: ${this.autoDiscover}`);
-    if (this.lmsHost) this.log.info(`Configured LMS Host: ${this.lmsHost}:${this.lmsPort}`);
+    this.log('LMSPlatform initialising...');
+    this.log('Config:', this.config);
 
     if (api) {
-      this.api.on('didFinishLaunching', () => {
-        this.log.info('Homebridge finished launching');
-
-        if (this.autoDiscover) {
-          this.discoverLMS();
-        } else if (this.lmsHost) {
-          this.connectToLMS(this.lmsHost, this.lmsPort);
-        } else {
-          this.log.error('No LMS host configured and autoDiscover disabled');
-        }
+      api.on('didFinishLaunching', () => {
+        this.log('Homebridge finished launching.');
+        this.discoverDevices();
       });
     }
   }
 
-  configureAccessory(accessory) {
-    // cached accessories restore
-    this.log.info(`Restoring cached accessory: ${accessory.displayName}`);
-  }
-
-  discoverLMS() {
-    this.log.info('Starting LMS auto-discovery...');
-
-    const socket = dgram.createSocket('udp4');
-    socket.bind(3483, () => {
-      socket.setBroadcast(true);
-      const message = Buffer.from('eIPAD\0NAME\0JSON\0VERS\01.0\0');
-      socket.send(message, 0, message.length, 3483, '255.255.255.255');
-    });
-
-    socket.on('message', (msg, rinfo) => {
-      const data = msg.toString();
-      if (this.debug) this.log.info(`Discovery response from ${rinfo.address}: ${data}`);
-
-      if (!this.lmsHost) {
-        this.lmsHost = rinfo.address;
-        this.log.info(`Discovered LMS at ${this.lmsHost}:${this.lmsPort}`);
-        this.connectToLMS(this.lmsHost, this.lmsPort);
-      }
-    });
-
-    setTimeout(() => {
-      socket.close();
-      if (!this.lmsHost) {
-        this.log.error('No LMS servers discovered');
-      }
-    }, 5000);
-  }
-
-  connectToLMS(host, port) {
-    this.log.info(`Connecting to LMS at ${host}:${port}`);
-    // next stage: JSON-RPC handshake + player enumeration
-  }
-
   discoverDevices() {
-    return [];
+    this.log('Starting LMS auto-discovery...');
+
+    if (this.config.autodiscover) {
+      this.log('Auto-discovery enabled');
+    }
+
+    if (this.config.server) {
+      this.log(`Using LMS server: ${this.config.server}`);
+    }
+
+    // placeholder — we’ll add LMS discovery logic next
   }
 }
-
-module.exports = (api) => {
-  Accessory = api.platformAccessory;
-  Service = api.hap.Service;
-  Characteristic = api.hap.Characteristic;
-  UUIDGen = api.hap.uuid;
-
-  api.registerPlatform(
-    'homebridge.v2.-mysqueezebox',  // MUST match package.json "name"
-    'LMSPlatform',                 // MUST match config.json "platform"
-    LMSPlatform
-  );
-};
