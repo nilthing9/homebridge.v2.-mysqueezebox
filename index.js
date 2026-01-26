88a4d491d00b8a1090c50eafc57793df122b8c69
nilthing9 <44004681+nilthing9@users.noreply.github.com>
2026-01-26 18:48:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 'use strict';
 
-let Service, Characteristic;
-
-module.exports = (api) => {
-  Service = api.hap.Service;
-  Characteristic = api.hap.Characteristic;
-
-  api.registerPlatform(
-    'homebridge-mysqueezebox-v2',   // must match package.json name
-    'LMSPlatform',                  // must match config.json platform
-    LMSPlatform
-  );
-};
+let Service, Characteristic, apiRef;
 
 class LMSPlatform {
   constructor(log, config, api) {
     this.log = log;
-    this.config = config || {};
+    this.config = config;
     this.api = api;
-
-    this.log('LMSPlatform initialising...');
-    this.log('Config:', this.config);
-
-    if (api) {
-      api.on('didFinishLaunching', () => {
-        this.log('Homebridge finished launching.');
-        this.discoverDevices();
-      });
-    }
+    this.log('LMSPlatform initialised');
   }
 
-  discoverDevices() {
-    this.log('Starting LMS auto-discovery...');
-
-    if (this.config.autodiscover) {
-      this.log('Auto-discovery enabled');
-    }
-
-    if (this.config.server) {
-      this.log(`Using LMS server: ${this.config.server}`);
-    }
-
-    // placeholder — we’ll add LMS discovery logic next
-  }
+  configureAccessory(accessory) {}
 }
+
+module.exports = (api) => {
+  apiRef = api;
+  Service = api.hap.Service;
+  Characteristic = api.hap.Characteristic;
+
+  api.registerPlatform(
+    'homebridge-mysqueezebox-v2',   // package.json name
+    'LMSPlatform',                  // config.json platform
+    LMSPlatform
+  );
+};
